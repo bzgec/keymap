79ef2bb2847ebbc67afdb13ec5637ff09cc11c9b
bzgec <luclul103@gmail.com>
2021-12-01 20:49:08 +0100

    First commit

---
+++ keymap.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euo pipefail  # https://gist.github.com/maxisam/e39efe89455d26b75999418bf60cf56c
+
+configFolder="${HOME}/.config/keymap"
+currKeymapFile="${configFolder}/currKeymap.txt"
+# mkdir -p ~/.config/keymap/ && cp keymap.sh ~/.config/keymap/
+
+defaultKeymap="si"
+defaultCustomKeymap="si_colemak"
+
+if [ $# -eq 0 ]; then
+    # No intput parameter - toggle
+    if [ ! -f "${currKeymapFile}" ]; then
+        # File doesn't exits -> create it with default keymap
+        echo "Current keymap file doesn't exist - creating new one."
+        scriptParam="${defaultKeymap}"
+    else
+        # File exists
+        currKeymap=$(cat ${currKeymapFile})
+        echo "Current keymap file content: ${currKeymap}"
+        if [ "${currKeymap}" == "${defaultKeymap}" ]; then
+            scriptParam="${defaultCustomKeymap}"
+        else
+            scriptParam="${defaultKeymap}"
+        fi
+    fi
+elif [ "$1" != "${defaultCustomKeymap}" ]; then
+    scriptParam="${defaultKeymap}"
+else
+    scriptParam="${defaultCustomKeymap}"
+fi
+
+echo "Setting '${scriptParam}'"
+echo "${scriptParam}" > "${currKeymapFile}"
+
+# Setup the keymapping
+setxkbmap "${scriptParam}"
